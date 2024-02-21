a988634b3132668b69f222c8c815daa4416464ff
552020 <stefanolombardo@posteo.de>
2024-02-21 13:35:32 +0100

    small changes

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -22,8 +22,7 @@ class Contact
 			const std::string &darkestSecret);
 	~Contact();
 
-	// Getters
-	std::string getFirstName() const; // For the meaning of const see Notes.
+	std::string getFirstName() const;
 	std::string getLastName() const;
 	std::string getNickname() const;
 	std::string getPhoneNumber() const;
@@ -31,18 +30,3 @@ class Contact
 };
 
 #endif
-
-// Notes:
-
-// The const at the end of a member function in a class declaration signifies
-// that the member function is a constant member function. When a member
-// function is declared as const, it means that the function does not modify any
-// non-static data members of the object for which it is called. Additionally,
-// within the scope of a const member function, the this pointer (which points
-// to the object on which the member function was invoked) is treated as a
-// pointer to a const object, so you can't call any non-const member functions
-// on it. This means that all "member function" or "method' which are "getters;
-// will be const.
-
-// If the function which is marked as 'const' will modify the class member,
-// which it is a function of we will get a compile-time error
